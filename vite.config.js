98e41a2341266156bac1cec45a68f385fc83f74f
Vilina Osilova <vilinaosilova@gmail.com>
2021-07-15 18:06:09 +0400

    changed assets links

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/movie-search/'
-    : '/',
+  publicPath: '/movie-search/',
   base: '',
   build: {
     rollupOptions: {
