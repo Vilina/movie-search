fedddca98d9394b1c678605b79291582eb17d464
Vilina Osilova <vilinaosilova@gmail.com>
2021-07-15 20:48:05 +0400

    Hopefully final build

--- src/config.js
+++ src/config.js
@@ -1,18 +1,15 @@
+let env = 'development';
+
+if (window.location.hostname === 'vilina.github.io') {
+  env = 'production';
+}
+
 let config = {
-  // baseUrl: '',
-  // apiUrl: '', //mock api
-  apiUrl: '/movie-search',
-  baseUrl: '/movie-search',
+  baseUrl: env==='production'?'/movie-search':'',
+  apiUrl: env==='production'?'/movie-search':'',
   moviesEndpoint: '/movies.json',
   actorsEndpoint: '/actors.json',
   genreEndpoint: '/genre.json'
 };
-
-// config = {
-//   apiUrl: 'https://app.pex-ms.com/', //mock api
-//   moviesEndpoint: 'api/movies',
-//   actorsEndpoint: 'api/actors',
-//   genreEndpoint: 'api/genre'
-// };
 
 export default config
--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import store from './_store/store'
 import router from "./router"
 import worker from './_api/mock/browser'
 
-worker.start({
-  onUnhandledRequest: 'bypass',
-  serviceWorker: {
-    url: '/movie-search/mockServiceWorker.js'
-  }
-})
-  .then(() => {;
+let action;
+if(process.env.NODE_ENV === 'production' && window.location.hostname === 'vilina.github.io') {
+  action = worker.start({
+    onUnhandledRequest: 'bypass',
+    serviceWorker: {
+      url: '/movie-search/mockServiceWorker.js'
+    }
+  })
+} else {
+  action = worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+action
+  .then(() => {
     const app = createApp(App);
     app.use(store);
     app.use(router);
